9da19db2abe3aad0e5e19303a4c9ad0f1feaa96b
lowpolysurf <lowpolysurf@gmail.com>
2026-04-01 14:17:36 -0700

    update

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -18,13 +18,63 @@ public class Reader{
         this.CARD_NUMBER_ = CARD_NUMBER_;
         this.NAME_ = NAME_;
         this.PHONE_ = PHONE_;
+        this.BOOK_COUNT_ = 0;
         this.books = new ArrayList<>();
     }
 
     //methods
+    public int getCardNumber() {
+        return CARD_NUMBER_;
+    }
+
+    public void setCardNumber(int CARD_NUMBER_) {
+        this.CARD_NUMBER_ = CARD_NUMBER_;
+    }
+
+    public String getName() {
+        return NAME_;
+    }
+
+    public void setName(String NAME_) {
+        this.NAME_ = NAME_;
+    }
+
+    public String getPhone() {
+        return PHONE_;
+    }
+
+    public void setPhone(String PHONE_) {
+        this.PHONE_ = PHONE_;
+    }
+
+    public int getBookCount() {
+        return BOOK_COUNT_;
+    }
+
+    public void setBookCount(int BOOK_COUNT_) {
+        this.BOOK_COUNT_ = BOOK_COUNT_;
+    }
+
+    public int getBookStart() {
+        return BOOK_START_;
+    }
+
+    public void setBookStart(int BOOK_START_) {
+        this.BOOK_START_ = BOOK_START_;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
     public Code addBook(Book add){
         if(!books.contains(add)){
             books.add(add);
+            BOOK_COUNT_++;
             return Code.SUCCESS;
         }
         return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
@@ -33,6 +83,7 @@ public class Reader{
     public Code removeBook(Book remove){
         if(books.contains(remove)){
             books.remove(remove);
+            BOOK_COUNT_--;
             return Code.SUCCESS;
         }
         return Code.READER_DOESNT_HAVE_BOOK_ERROR;
--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,4 +1,7 @@
+import Utilities.Code;
 
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Shelf{
 
@@ -9,8 +12,65 @@ public class Shelf{
     protected HashMap<Book, Integer> books;
 
     //constructor
+    public Shelf() {
+    }
+    public Shelf(int SHELF_NUMBER_, String SUBJECT_) {
+        this.SHELF_NUMBER_ = SHELF_NUMBER_;
+        this.SUBJECT_ = SUBJECT_;
+    }
 
     //methods
+    public int getShelfNumber() {
+        return SHELF_NUMBER_;
+    }
 
+    public void setShelfNumber(int SHELF_NUMBER_) {
+        this.SHELF_NUMBER_ = SHELF_NUMBER_;
+    }
 
+    public String getSubject() {
+        return SUBJECT_;
+    }
+
+    public void setSubject(String SUBJECT_) {
+        this.SUBJECT_ = SUBJECT_;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Shelf shelf = (Shelf) o;
+        return SHELF_NUMBER_ == shelf.SHELF_NUMBER_ && Objects.equals(SUBJECT_, shelf.SUBJECT_);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(SHELF_NUMBER_, SUBJECT_);
+    }
+
+    @Override
+    public String toString() {
+        return SHELF_NUMBER_ + " : " + SUBJECT_;
+    }
+
+    public int getBookCount(Book book){
+        if(this.books.containsKey(book)){
+            return this.books.get(book);
+        }
+        return 0;
+    }
+
+    public Code addBook(Book book){
+        if(this.books.containsKey(book)){
+            int tempCount = this.books.get(book) + 1;
+            this.books.put(book, tempCount);
+            System.out.println(book.toString() + " added to shelf " + this.toString());
+            return Code.SUCCESS;
+        }else if(book.getSubject().equals(this.SUBJECT_)){
+            this.books.put(book, 1);
+            System.out.println(book.toString() + " added to shelf " + this.toString());
+            return Code.SUCCESS;
+        }
+        return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+    }
 }
